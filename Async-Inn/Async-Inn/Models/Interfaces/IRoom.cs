c8faee7b11a070af05dd29cee66b941edc2cb646
HasanMufdy <96997047+HasanMufdy@users.noreply.github.com>
2023-07-24 00:04:17 +0300

    added services and interfaces for the 3 models

---
+++ Async-Inn/Async-Inn/Models/Interfaces/IRoom.cs
@@ -0,0 +1,14 @@
+﻿namespace Async_Inn.Models.Interfaces
+{
+    public interface IRoom
+    {
+        Task<Room> Create(Room room);
+        Task<List<Room>> GetRooms();
+
+        Task<Room> GetRoom(int roomId);
+
+        Task<Room> UpdateRoom(int id, Room room);
+
+        Task Delete(int id);
+    }
+}
---
+++ Async-Inn/Async-Inn/Models/Services/RoomService.cs
@@ -0,0 +1,48 @@
+﻿using Async_Inn.Data;
+using Async_Inn.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Async_Inn.Models.Services
+{
+    public class RoomService : IRoom
+    {
+        private readonly HotelDbContext _context;
+
+        public RoomService(HotelDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Room> Create(Room room)
+        {
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+            return room;
+        }
+
+        public async Task Delete(int id)
+        {
+            Room room = await GetRoom(id);
+            _context.Entry(room).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Room> GetRoom(int roomId)
+        {
+            Room room = await _context.Rooms.FindAsync(roomId);
+            return room;
+        }
+
+        public async Task<List<Room>> GetRooms()
+        {
+            var rooms = await _context.Rooms.ToListAsync();
+            return rooms;
+        }
+
+        public async Task<Room> UpdateRoom(int id, Room room)
+        {
+            _context.Entry(room).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return room;
+        }
+    }
+}
